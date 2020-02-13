f963dd0a6f5b256bf53d2faf158423f9ea8743ed
richknicks <rlynch@gmail.com>
2020-02-13 17:22:55 -0500

    test passed on App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,14 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders App without crashing", () => {
-  render(<App />);
+// test("renders App without crashing", () => {
+//   const { getByText } = render(<App />);
+//   const header = getByText(//i);
+//   expect(header).toBeInTheDocument();
+//   expect(header).toBeTruthy();
+//   expect(header).not.toBeFalsy();
+// });
+test('concise test - renders contact form header', () => {
+  const {getByText } = render(<App />);
+  getByText(/first name/i);
 });
